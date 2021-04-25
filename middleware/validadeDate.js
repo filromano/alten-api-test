a53c1062c8a0117d812d1166f0b21578399926dd
Filipe Romano <filromano@gmail.com>
2021-04-25 17:24:33 -0300

    UPDATE: validate time format

--- middleware/validadeDate.js
+++ middleware/validadeDate.js
@@ -1,7 +1,14 @@
 const validateDate = (req, res, next) => {
+    const pattern = '00:00:00.000Z';
     if(isNaN(Date.parse(req.body.checkIn)) || isNaN(Date.parse(req.body.checkOut))) {
-        res.status(500).json({message: 'Not valid format'})
+        res.status(400).json({message: 'Invalid format'})
     } else {
+        // check time format
+        let checkIn = req.body.checkIn.split('T');
+        let checkOut = req.body.checkOut.split('T');
+        if (checkIn[1] !== pattern || checkOut[1] !== pattern) {
+            res.status(400).json({message: 'Invalid format'})
+        }
         next();
     }
 };
